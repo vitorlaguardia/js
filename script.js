4691e136d0652bf245c68502a2044856ebd91f1f
vitorlaguardia <146145376+vitorlaguardia@users.noreply.github.com>
2024-05-02 08:37:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function perguntaridade() {
+    var idade = prompt ("Qual sua idade?");
+    alert("A sua idade é " + idade)
+}
+
+function perguntarnome() {
+    var nome = prompt ("Qual seu nome?");
+    alert("O seu nome é " + nome)
+}
